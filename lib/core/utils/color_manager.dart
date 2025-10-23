ea631765022175562d73bddad30f837f9d4f9a12
hossamgado <hossammahmoudgado@gmail.com>
2025-10-23 14:41:42 +0300

    create color manager

---
+++ lib/core/utils/color_manager.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorManager{
+
+  static Color primary=Color(0xff004182);
+  static Color primaryDark=Color(0xff06004F);
+  static Color appBarTitleColor=Color(0xff06004F).withValues(alpha: 0.6);
+  static Color white=Color(0xffFFFFFF);
+  static Color black = const Color(0xff000000);
+  static Color transparent=Colors.transparent;
+  static Color darkPrimary = const Color(0xffd17d11);
+  static Color containerGray = const Color(0xffDBE4ED);
+  static Color yellow = const Color(0xFFFDD835);
+}
